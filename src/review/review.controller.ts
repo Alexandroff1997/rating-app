bb5c065999925a86809cbcba9464d29c04c1fdbc
Alex <symire.alexander1997@gmail.com>
2024-08-08 15:25:46 +0700

    decorator for getting user

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,6 +16,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVEIEW_NOT_FOUND as REVIEW_NOT_FOUND } from './common/review-constants';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -39,9 +40,15 @@ export class ReviewController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
   @HttpCode(200)
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {
+  async getByProduct(
+    @Param('productId') productId: string,
+    @UserEmail() email: string,
+  ) {
+    console.log(email);
+
     return this.reviewService.findByProductId(productId);
   }
 
